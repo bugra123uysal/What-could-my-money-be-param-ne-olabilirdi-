f0bf89c91837fa09ae75c6f10611c8ff7b9a22e2
buğra uysal <uysalbugra134@gmail.com>
2024-10-27 23:18:44 +0300

    The percentage change of the stock since the beginning of the year will be added to the user's entered budget to calculate the compound interest.

--- try.py
+++ try.py
@@ -5,7 +5,9 @@ import numpy as np
 import yfinance as yf
 import tkinter as tk
 import datetime
-hisseler = ['AAPL', 'MSFT', 'GOOGL', 'TSLA', 'AMZN', 'NFLX', 'NVDA', 'PYPL', 'BABA']
+
+
+
 
 
 maıın=tk.Tk()
@@ -13,17 +15,57 @@ maıın=tk.Tk()
 maıın.title("dene")
 
 maıın.geometry("500x500")
+hisseler = ['AAPL', 'MSFT', 'GOOGL', 'TSLA', 'AMZN', 'NFLX', 'NVDA', 'PYPL', 'BABA']
+""" değişken tanımlama """
+sec_hısse=tk.StringVar()
+sec_hısse.set(hisseler[0])
 
-mnysv=tk.Entry(maıın, textvariable='birikim miktarını giriniz')
+
+   
+   
+
+
+def grfk():
+   global bucket 
+   global yy
+
+   
+   """ bütcenin yüzdesini almak  almak  """
+ 
+   bucket=float( mnysv.get())
+   mn=sec_hısse.get()
+   
+
+   strt=datetime.datetime.now()
+   sn=datetime.datetime(strt.year , 1,1)
+
+   sok=yf.download(mn, start=sn , end=strt)
+
+   yılbs=sok['Close'].iloc[0]
+   gncl=sok['Close'].iloc[-1]
+
+   """ hisse senedinin yılbaşından bu yana  yüzde getirisi  """
+   yy=((gncl-yılbs)/yılbs)*100
+
+   bte=(bucket *(1 + (yy /100)))
+
+
+   lbl=tk.Label(maıın, text=f"{mn}: {yy} , \n  {bucket} birim para , yükselerek  {bte} olur   "  , font=('Helvetica', 12))
+   lbl.pack()
+
+
+
+
+
+mnysv=tk.Entry(maıın)
 mnysv.pack()
 
-oe=tk.OptionMenu(maıın, *hisseler )
+oe=tk.OptionMenu(maıın, sec_hısse ,*hisseler )
 oe.pack()
 
 
-hspl=tk.Button(maıın, text='hesapla')
+hspl=tk.Button(maıın, text='hesapla', command=grfk)
 hspl.pack()
-
 
 
 maıın.mainloop()
